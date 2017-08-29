bf71a9fa559ce064ba5a3e3690e7839fa7926d4b
Nutti Saelor <leelorz6@gmail.com>
2017-08-29 17:30:04 +0700

    add header content style and thumbnail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,19 +1,35 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 
 const AlbumDetail = (props) => {
+  const { title, artist, thumbnail_image } = props.album
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
-      </CardSection>
-      <CardSection>
-        <Text>{props.album.artist}</Text>
+        <View>
+          <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   )
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
 }
 
 export default AlbumDetail
