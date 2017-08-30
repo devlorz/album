2bfc2168a2a4e0bbb4e1aaf68af32c6f335e3557
Nutti Saelor <leelorz6@gmail.com>
2017-08-30 17:03:07 +0700

    fix styles and add album artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,18 +4,31 @@ import Card from './Card'
 import CardSection from './CardSection'
 
 const AlbumDetail = (props) => {
-  const { title, artist, thumbnail_image } = props.album
+  const { title, artist, thumbnail_image, image } = props.album
+  const { 
+    thumbnailStyle, 
+    headerContentStyle,  
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles
 
   return (
     <Card>
       <CardSection>
-        <View>
-          <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
-        <View style={styles.headerContentStyle}>
-          <Text>{title}</Text>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={imageStyle} 
+          source={{ uri: image }} 
+        />
       </CardSection>
     </Card>
   )
@@ -26,9 +39,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 
